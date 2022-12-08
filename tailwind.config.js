64ee6000b7a50a2c3f4ea9bc0ae38cbc9da1c725
alphabeticalist <alphbeticalist@gmail.com>
2022-12-08 08:54:33 +0100

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+
+      fontFamily: {
+        inter: ["Inter, sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
